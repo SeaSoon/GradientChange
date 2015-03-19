29ed42807858a9f3678187e846b05ffa3ffe99eb
Sizon <snow_ocean@163.com>
2015-03-19 16:28:50 +0800

    fragment
    简单的fragment

---
+++ src/com/ocean/gradualchange/ImageFragment.java
@@ -0,0 +1,26 @@
+package com.ocean.gradualchange;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+@SuppressLint("NewApi")
+public class ImageFragment extends Fragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.item, null);
+		return view;
+	}
+}
